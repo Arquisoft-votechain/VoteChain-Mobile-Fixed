169a48db4a593ed80780f114f70ef3768ea77ad2
UsuarioDeCodigo <86996273+UsuarioDeCodigo@users.noreply.github.com>
2023-06-16 00:36:45 -0500

    .

--- primer_proyecto/lib/modules/user-manager/user-views/vote.dart
+++ primer_proyecto/lib/modules/user-manager/user-views/vote.dart
@@ -7,19 +7,178 @@ import '../user-views/vote-if-not.dart';
 import 'package:http/http.dart' as http;
 
 
-import 'dart:convert';
-import 'package:shared_preferences/shared_preferences.dart';
+/*class VoteView extends StatelessWidget {
+ @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(        
 
-import 'package:flutter/material.dart';
-import 'package:primer_proyecto/modules/user-manager/models/archivo.dart';
-import 'package:primer_proyecto/modules/vote-management/vote.service.dart';
-import '../../../themes/color.dart';
-import '../user-views/vote-if-not.dart';
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:http/http.dart' as http;
-import 'dart:async';
+        appBar: AppBar(
+          title: Text('VOTACION'),
+        ),
 
+
+        body: SingleChildScrollView(
+          child: Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(color: Color(0xFFED3131), width: 1),
+                    color: Colors.white,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(right: 20),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(50),
+                                child: Image.asset(
+                                  'assets/img/image1.jpg',
+                                  width: 70,
+                                  height: 70,
+                                ),
+                              ),
+                            ),
+
+                            Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'NOMBRE DE PARTIDO POLITICO',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                Text(
+                                  'Alcalde: Angel Velasquez Nuñez',
+                                  textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          width: double.infinity,
+                          child: FractionallySizedBox(
+                            widthFactor: 0.5,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => VoteIfNotView()));
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xFF3F468F),
+                              ),
+                              child: Text('VOTE', style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              
+              SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(color: Color(0xFFED3131), width: 1),
+                    color: Colors.white,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(right: 20),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(50),
+                                child: Image.asset(
+                                  'assets/img/image1.jpg',
+                                  width: 70,
+                                  height: 70,
+                                ),
+                              ),
+                            ),
+                            Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'NOMBRE DE PARTIDO POLITICO',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                Text(
+                                  'Alcalde: Angel Velasquez Nuñez',
+                                  textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          width: double.infinity,
+                          child: FractionallySizedBox(
+                            widthFactor: 0.5,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Acción al presionar el botón
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xFF3F468F),
+                              ),
+                              child: Text('VOTE', style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 100) //spacer final
+            ],
+          ),
+        ),
+        ),
+      ),
+    );
+  }
+}*/
 
 class VoteView extends StatefulWidget {
   @override
@@ -27,8 +186,6 @@ class VoteView extends StatefulWidget {
 }
 
 class _VoteViewState extends State<VoteView> {
-  late ListVoteService listPublicationesService= ListVoteService();
-
   List users = [];
   bool isLoading = false;
   @override
@@ -41,10 +198,11 @@ class _VoteViewState extends State<VoteView> {
     setState(() {
       isLoading = true;
     });
-    var url = Uri.parse('http://192.168.1.8:3000/schools/1/master-political-parties');
+    var url = Uri.parse('https://randomuser.me/api/?results=10');
     var response = await http.get(url);
+    // print(response.body);
     if(response.statusCode == 200){
-      var items = json.decode(response.body);
+      var items = json.decode(response.body)['results'];
       setState(() {
         users = items;
         isLoading = false;
@@ -68,15 +226,16 @@ class _VoteViewState extends State<VoteView> {
       return Center(child: CircularProgressIndicator(valueColor: new AlwaysStoppedAnimation<Color>(primary),));
     }
     return ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (context,index){
-          return getCard(users[index]);
-        });
+      itemCount: users.length,
+      itemBuilder: (context,index){
+      return getCard(users[index]);
+    });
   }
   Widget getCard(item) {
-    var fullName = item['name'];
-    var email = item['description'];
-    var masterPoliticalPartyId = item['id'];
+    var fullName = item['name']['title'] + " " + item['name']['first'] + " " + item['name']['last'];
+    var email = item['email'];
+    var profileUrl = item['picture']['large'];
+    //image: NetworkImage(profileUrl)
 
     return Card(
       elevation: 1.5,
@@ -87,7 +246,7 @@ class _VoteViewState extends State<VoteView> {
             ListTile(
               title: Row(
                 children: <Widget>[
-                  /*Container(
+                  Container(
                     width: 60,
                     height: 60,
                     decoration: BoxDecoration(
@@ -98,7 +257,7 @@ class _VoteViewState extends State<VoteView> {
                         image: NetworkImage(profileUrl),
                       ),
                     ),
-                  ),*/
+                  ),
                   SizedBox(width: 20),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -122,29 +281,23 @@ class _VoteViewState extends State<VoteView> {
             ),
             SizedBox(height: 10),
             Container(
-              width: double.infinity,
-              child: FractionallySizedBox(
-                widthFactor: 0.5,
-                //boton
-                child: ElevatedButton(
-                  onPressed: () {
-                    print("vamos a relaizar el voto");
-                    print(masterPoliticalPartyId);
-                    listPublicationesService.postVote(2,masterPoliticalPartyId);
-                    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => VoteIfNotView()));
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF3F468F),
-                  ),
-                  child: Text('VOTE', style: TextStyle(color: Colors.white)),
-                ),
-              ),
-            ),
+                          width: double.infinity,
+                          child: FractionallySizedBox(
+                            widthFactor: 0.5,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => VoteIfNotView()));
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xFF3F468F),
+                              ),
+                              child: Text('VOTE', style: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                        ),
           ],
         ),
       ),
     );
   }
 }
-
-
